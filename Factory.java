66c7ae3d7fbd4456d7baa064b2933aa36ded57d6
haidarjakiem <haidardzaky333@gmail.com>
2023-10-18 06:19:45 +0700

    perkembangan bolo

--- Factory.java
+++ Factory.java
@@ -1,65 +1,25 @@
-import greenfoot.*;  
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-
-public class Factory extends World
+public class Factory extends Main
 {
-    private GreenfootImage backgroundImage;
-    private int scrollSpeed = 1;
-    private int scrollPosition = 0;
-    
-    Counter counter = new Counter();
-    Energy energy = new Energy();
-    Highscore highscore = new Highscore();
-    
-    private int timer = 0;
-    private void paint(int position)
-    {
-        getBackground().drawImage(backgroundImage, position - backgroundImage.getWidth(), 0);
-        getBackground().drawImage(backgroundImage, position, 0);
-    }
     public Factory()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, false);
+    {
         backgroundImage = new GreenfootImage("bege.png");
         setBackground(backgroundImage);
         prepare();
     }
     
-    public Counter getCounter(){
-        return counter;        
-    }
-    
-    public Energy getEnergy(){
-        return energy;
-    }
-    
-    public Highscore getHighscore() {
-        return highscore;
-    }
-    
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
-    {
-        addObject(counter, 75, 40);
-        addObject(energy, 500, 40);
-
+    private void prepare(){
         Block block1 = new Block();
-        addObject(block1,300 , 203);
+        addObject(block1, 300, 203);
         Block block2 = new Block();
         addObject(block2, 300, 376);
-
-        Cute cute = new Cute();
-        addObject(cute,111,327);
         
+        addObject(cute, 111, 327);
     }
     
     public void act(){
         timer++;
-        
         if(timer % 300 == 0 ){
             addObject(new Dot(), 600, 330);
             addObject(new Dot(), 600, 155);
@@ -74,10 +34,5 @@ public class Factory extends World
             scrollPosition = (scrollPosition - scrollSpeed) % getWidth();
             paint(scrollPosition);
         }
-        
-
-        if(Greenfoot.isKeyDown("escape")){
-            Greenfoot.setWorld(new gameMenu());
-        }
-    }   
+    }
 }
