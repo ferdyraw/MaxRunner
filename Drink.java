c3ec229e769b873e22247235475d971c2f4d7a28
Ferdy Rizkiawan <duniaferdy@gmail.com>
2023-10-17 17:45:02 +0700

    new character

--- Drink.java
+++ Drink.java
@@ -6,16 +6,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Coin extends Actor
+public class Drink extends Actor
 {
-    private GreenfootImage[] imagesCoin= new GreenfootImage[10];
+    private GreenfootImage[] imagesDrink= new GreenfootImage[10];
     /**
      * Act - do whatever the coin wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int countCoin = 0;
-    public Coin(){
-        animCoin();
+    private int countDrink = 0;
+    public Drink(){
+        animDrink();
     }
     public void act()
     {
@@ -24,14 +24,14 @@ public class Coin extends Actor
         // Add your action code here.
     }
     
-    public void animCoin(){
+    public void animDrink(){
         for(int i = 0; i < 10; i++){
             int a = i+1;
             String filename = "coin"+ a + ".png";
-            imagesCoin[i] = new GreenfootImage(filename);   
+            imagesDrink[i] = new GreenfootImage(filename);   
         } 
     }
     public void Anim(){
-        setImage(imagesCoin[countCoin++ %10]);
+        setImage(imagesDrink[countDrink++ %10]);
     }
 }
