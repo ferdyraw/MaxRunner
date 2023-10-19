b5e48a9bdc867cf46afbf9e824325823636c5c04
humamalwi <alwia901@gmail.com>
2023-10-19 19:57:33 +0700

    story setengah jadi

--- Coolinstory.java
+++ Coolinstory.java
@@ -12,15 +12,25 @@ public class Coolinstory extends Cool
      * Act - do whatever the Coolinstory wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int timer = 0;
     private int num = 2;
     public void act()
     {
+        timer++;
         setLocation(getX() + num, getY());
         
         if(getX() == 150){
             num = 0;
         } else Running();
+        
+        if(timer == 350){
+            num = 3;
+        }
+        if(getX() > 600){
+            Greenfoot.setWorld(new Lab());
+        }
         stop();
+        
     }
     public void stop(){
         if(num == 0){
